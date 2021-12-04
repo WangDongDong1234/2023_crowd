1d0e55fde1744de2d1c21be2acca2a6b138db99f
WangDongDong1234 <827495316@qq.com>
2021-12-04 22:16:40 +0800

    feat(20211204):02模块war打的包才包含依赖文件

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/entity/mapper/AdminMapper.java
@@ -0,0 +1,18 @@
+package com.atguigu.crowd.entity.mapper;
+
+
+import com.atguigu.crowd.entity.entity.AdminEntity;
+
+public interface AdminMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(AdminEntity record);
+
+    int insertSelective(AdminEntity record);
+
+    AdminEntity selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(AdminEntity record);
+
+    int updateByPrimaryKey(AdminEntity record);
+}
