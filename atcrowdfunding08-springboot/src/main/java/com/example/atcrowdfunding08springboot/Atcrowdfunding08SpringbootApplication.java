2bbcb3d8ea9a5edc2dd895b3f167536ce5c427c3
WangDongDong1234 <827495316@qq.com>
2022-06-01 00:40:38 +0800

    feat(220531)：springboot项目初始化

---
+++ atcrowdfunding08-springboot/src/main/java/com/example/atcrowdfunding08springboot/Atcrowdfunding08SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.atcrowdfunding08springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atcrowdfunding08SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Atcrowdfunding08SpringbootApplication.class, args);
+    }
+
+}
---
+++ atcrowdfunding08-springboot/src/test/java/com/example/atcrowdfunding08springboot/Atcrowdfunding08SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atcrowdfunding08springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atcrowdfunding08SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
