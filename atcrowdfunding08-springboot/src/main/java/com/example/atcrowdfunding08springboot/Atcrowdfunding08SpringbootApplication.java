efb1e3e36e9751d273170a603fa4b7804905e8b0
WangDongDong1234 <827495316@qq.com>
2022-06-04 01:24:16 +0800

    feat(220604):@MapperScan和@ComponentScan区别与使用方法

--- atcrowdfunding08-springboot/src/main/java/com/example/atcrowdfunding08springboot/Atcrowdfunding08SpringbootApplication.java
+++ atcrowdfunding08-springboot/src/main/java/com/example/atcrowdfunding08springboot/Atcrowdfunding08SpringbootApplication.java
@@ -4,13 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 @MapperScans({@MapperScan("com.example.atcrowdfunding08springboot.mapper")})
-@ComponentScans({
-        @ComponentScan("com.example.atcrowdfunding08springboot.config"),
-        @ComponentScan("com.example.atcrowdfunding08springboot.control")})
 @SpringBootApplication
 public class Atcrowdfunding08SpringbootApplication {
 
