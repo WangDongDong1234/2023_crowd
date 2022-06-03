5cd735dbaa3e892512225ba30fd35c6b75fd0d46
WangDongDong1234 <827495316@qq.com>
2022-06-04 00:33:38 +0800

    feat(220604):yml配置生效，@Value注解

--- atcrowdfunding08-springboot/src/test/java/com/example/atcrowdfunding08springboot/Atcrowdfunding08SpringbootApplicationTests.java
+++ atcrowdfunding08-springboot/src/test/java/com/example/atcrowdfunding08springboot/Atcrowdfunding08SpringbootApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -18,10 +19,17 @@ class Atcrowdfunding08SpringbootApplicationTests {
     @Autowired
     private Student student;
 
+    /**
+     * 仅支持简单类型
+     */
+    @Value("${atguigu.best.wishes}")
+    private String wishes;
+
     @Test
     public void testReadYaml() {
 
-        logger.info(student.toString());
+        logger.debug(student.toString());
+        logger.debug("wishes:"+wishes);
 
     }
 
