47cb6a607e6b566ed0e12f77ae33dbc2ca662947
WangDongDong1234 <827495316@qq.com>
2022-06-05 16:54:10 +0800

    feat(220604):springboot和redis整合。测试类启动，需要启动redis.

--- atcrowdfunding08-springboot/src/test/java/com/example/atcrowdfunding08springboot/Atcrowdfunding08SpringbootApplicationTests.java
+++ atcrowdfunding08-springboot/src/test/java/com/example/atcrowdfunding08springboot/Atcrowdfunding08SpringbootApplicationTests.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -30,6 +31,9 @@ class Atcrowdfunding08SpringbootApplicationTests {
     @Value("${atguigu.best.wishes}")
     private String wishes;
 
+//    @Autowired
+//    private RedisTemplate<String,String > redisTemplate;
+
     @Test
     public void testReadYaml() {
 
@@ -44,4 +48,12 @@ class Atcrowdfunding08SpringbootApplicationTests {
         logger.debug("adminEntity:"+adminEntity);
     }
 
+    /**
+     * 该测试类需要把redis启动起来
+     */
+    @Test
+    public void testRedis(){
+//        redisTemplate.opsForValue().set("hello","world");
+//        System.out.println(redisTemplate.opsForValue().get("hello"));
+    }
 }
