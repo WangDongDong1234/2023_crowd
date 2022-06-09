e15d6a6590203d8555dde6dbb5000a05b031ebab
WangDongDong1234 <827495316@qq.com>
2022-06-10 03:02:35 +0800

    feat(220610):访问不同provider集群

--- spring-cloud-study-parent/spring-cloud-study-provider/src/main/java/com/atguigu/springcloud/provider/handler/HelloHandler.java
+++ spring-cloud-study-parent/spring-cloud-study-provider/src/main/java/com/atguigu/springcloud/provider/handler/HelloHandler.java
@@ -4,11 +4,14 @@ import com.atugui.spring.cloud.entity.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloHandler {
     
     @GetMapping("/provider/hello")
-    public Employee hello(){
-        return new Employee(1,"王冬冬",23.5);
+    public Employee hello(HttpServletRequest request){
+        int port = request.getServerPort();
+        return new Employee(1,"王冬冬"+port,23.5);
     }
 }
