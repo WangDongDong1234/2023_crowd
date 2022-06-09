6fcab8f741435391d94c1bbefaed269a945997d6
WangDongDong1234 <827495316@qq.com>
2022-06-10 02:05:51 +0800

    feat(220610):eureka服务器端启动

---
+++ spring-cloud-study-parent/spring-cloud-study-eureka/src/main/java/com/atguigu/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//启用eureka服务器功能
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
