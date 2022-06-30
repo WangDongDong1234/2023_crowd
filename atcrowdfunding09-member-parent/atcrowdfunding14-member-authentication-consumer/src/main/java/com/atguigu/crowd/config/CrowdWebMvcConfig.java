1389b9c1f6653779081dc7b1efeba4a8211a4e29
WangDongDong1234 <827495316@qq.com>
2022-07-01 01:16:14 +0800

    feat(220701):注册页面微调（前端页面引入学习使用）

--- atcrowdfunding09-member-parent/atcrowdfunding14-member-authentication-consumer/src/main/java/com/atguigu/crowd/config/CrowdWebMvcConfig.java
+++ atcrowdfunding09-member-parent/atcrowdfunding14-member-authentication-consumer/src/main/java/com/atguigu/crowd/config/CrowdWebMvcConfig.java
@@ -1,8 +1,10 @@
 package com.atguigu.crowd.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CrowdWebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
