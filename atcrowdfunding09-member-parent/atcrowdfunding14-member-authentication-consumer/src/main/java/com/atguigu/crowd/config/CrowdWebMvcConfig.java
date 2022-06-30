1d9ff45a2603ccec36c53d20a438b0b18917c3c8
WangDongDong1234 <827495316@qq.com>
2022-07-01 00:41:19 +0800

    feat(220701):1.url和viewname映射 2.直接添加注册页面（前端页面引入学习使用）

---
+++ atcrowdfunding09-member-parent/atcrowdfunding14-member-authentication-consumer/src/main/java/com/atguigu/crowd/config/CrowdWebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.atguigu.crowd.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CrowdWebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 浏览器访问的地址
+        String urlPath = "/auth/member/to/reg/page";
+
+        // 目标视图的名称，将来拼接“prefix: classpath:/templates/”、“suffix: .html”前后缀
+        String viewName = "member/member-reg";
+
+        // 添加view-controller
+        // 1 会员注册页面（填写个人信息）
+        registry.addViewController(urlPath).setViewName(viewName);
+        // 2 会员登录页面
+        registry.addViewController("/auth/member/to/login/page").setViewName("member/member-login");
+        // 3 登录成功界面
+        registry.addViewController("/auth/member/to/center/page").setViewName("member/member-center");
+    }
+}
